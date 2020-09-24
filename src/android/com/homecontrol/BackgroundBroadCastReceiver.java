5d2ec9abc2f3e134c010e9ba525141f6aade8860
Arpita <arpita.jena@empower-solutions.co.in>
2020-09-24 17:02:51 +0530

    fix Issue

--- src/android/com/homecontrol/BackgroundBroadCastReceiver.java
+++ src/android/com/homecontrol/BackgroundBroadCastReceiver.java
@@ -13,15 +13,15 @@ import com.heywatchme.backgroundwebsocket.foregroundservice.YourService;
 public class BackgroundBroadCastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.i("BroadcastListened", "Service tried to stop");
-        Toast.makeText(context, "Service restarted", Toast.LENGTH_SHORT).show();
+        Log.i("BroadcastListened", "Start ServiceS");
+       // Toast.makeText(context, "Service restarted", Toast.LENGTH_SHORT).show();
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
           //  context.startForegroundService(new Intent(context, YourService.class));
-            context.startService(new Intent(context, BackgroundBroadCastReceiver.class));
+            context.startService(new Intent(context, BackgroundService.class));
         } else {
 
-            context.startService(new Intent(context, BackgroundBroadCastReceiver.class));
+            context.startService(new Intent(context, BackgroundService.class));
         }
     }
 }
